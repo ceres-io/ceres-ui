66cfe9ec3ba8937446da8fdda4a5796d5112d383
ryan-hoang <rhoang@gmu.edu>
2020-04-22 02:40:46 -0400

    Added sign in/sign up buttons and remember me.

--- src/components/UserLogin/UserLogin.stories.tsx
+++ src/components/UserLogin/UserLogin.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
 import {SignIn} from './UserLogin'
 
-export default {title: 'Login Screen'}
+export default {title: 'Login Card'}
 export const login = () => <SignIn/>
--- src/components/UserLogin/UserLogin.tsx
+++ src/components/UserLogin/UserLogin.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Card, CardContent, CardHeader, Grid, makeStyles, TextField, Container, Avatar, LockOutlinedIcon} from "@material-ui/core"
+import { Card, CardContent, CardHeader, Grid, makeStyles, TextField, Container, Avatar, LockOutlinedIcon, Button, FormControlLabel, Checkbox} from "@material-ui/core"
+import LockIcon from '@material-ui/icons/Lock';
 
 const useStyles = makeStyles({
 
     root:{
            maxWidth:500,
-           minHeight: 500,
+           minHeight: 300,
            background: 'linear-gradient(60deg, #EEEEEE 30%, #E0E0E0 100%)'
         },
     grid: {
@@ -13,18 +14,27 @@ const useStyles = makeStyles({
         },
 })
 
+//TODO
+function onClickLoginHandler(){}
 
+//TODO
+function onClickSignUpHandler(){}
 
+//TODO
+function onChangeRememberMeHandler(){}
 
 export function SignIn() {
 const classes = useStyles()
   return (
   <Card className={classes.root}>
     <CardHeader title={"Login"} style={{textAlign:'center'}} />
-     <Avatar>
-     <LockOutlinedIcon/>
-     </Avatar>
-     <Container maxWidth='xs'>
+       <Grid
+         container
+         direction="column"
+         justify="space-between"
+         alignItems="center"
+       >
+        <Container maxWidth='xs'>
         <TextField
             variant="outlined"
             margin="none"
@@ -46,7 +56,25 @@ const classes = useStyles()
             id="password"
             autoComplete="current-password"
             />
+            <Grid
+              container
+              direction="row"
+              justify="space-evenly"
+              alignItems="center"
+            >
+            <Button variant="contained" color="primary" onClick={onClickLoginHandler}>
+              Sign In
+            </Button>
+             <Button variant="contained" color="primary" onClick={onClickSignUpHandler}>
+                          New Here? Sign Up Now!
+             </Button>
+
+            </Grid>
+            <FormControlLabel
+                           control={<Checkbox value="remember" color="primary" onChange={onChangeRememberMeHandler}/>}
+                           label="Remember me"/>
     </Container>
+    </Grid>
   </Card>
 
   );
