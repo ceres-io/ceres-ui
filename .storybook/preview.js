2dd1d9620ec9114db5edd91306a52ce386ab798a
mottley <claytonmottley@gmail.com>
2020-04-28 23:17:29 -0400

    wrap storybook components in themeprovider as well

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,12 +3,27 @@ import { createReduxStore } from '../src/redux/store/createStore';
 import { configureRouter } from '../src/routes/configureRouter';
 import { addDecorator } from '@storybook/react';
 import { Provider } from 'react-redux';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core';
 
 const router = configureRouter();
 const store = createReduxStore(router);
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#2e7d32'
+    },
+    secondary: {
+      main: '#7cb342'
+    }
+  }
+});
+
+
 addDecorator(s =>
   <Provider store={store}>
-    {s()}
+    <ThemeProvider theme={theme}>
+      {s()}
+    </ThemeProvider>
   </Provider>
 );
